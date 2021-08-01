d5c3d797d3f603b659ebfff1a1413a53a510f364
Arshad <jahan665577@gmail.com>
2021-08-01 13:58:16 -0700

    i change that to 2 table

--- first/bin/first.dart
+++ first/bin/first.dart
@@ -2,12 +2,4 @@ void main(List<String> arguments) {
   for (var i = 1; i < 10; i++) {
     print('$i *2 = ${i * 2}');
   }
-  print('*****************************');
-  print('================================');
-
-  for (var i = 0; i < 20; i++) {
-    if (i % 2 == 0) {
-      print('$i');
-    }
-  }
 }
