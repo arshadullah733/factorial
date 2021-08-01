2d129dd6a5debbf76bb3179c2c574445c61acf6b
Arshad <jahan665577@gmail.com>
2021-08-01 14:02:33 -0700

    also add even number

--- first/bin/first.dart
+++ first/bin/first.dart
@@ -2,4 +2,12 @@ void main(List<String> arguments) {
   for (var i = 1; i < 10; i++) {
     print('$i *2 = ${i * 2}');
   }
+  print('*****************************');
+  print('================================');
+
+  for (var i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+      print('$i');
+    }
+  }
 }
